85c7c3c02d7fa68ab99c972b02cf72aaaaafcdd2
Md. Saiful Islam <saiful9122113@gmail.com>
2022-09-29 04:33:30 +0600

    card section done

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = ({activity}) => {
+    // console.log(activity.title);
     return (
-        <div>
-            <h3>This is from card</h3>
+        <div>           
+            <div className='card'>
+                <img src={activity.img} alt="" />
+                <div className='card-info'>
+                    <h1>{activity.title}</h1>
+                    <p>Deviation time: {activity.watchTime}</p>
+                    <p>Required time: {activity.totalTime}</p>
+                </div>
+                <button className='btn-card'>
+                    <h2 className='btn-text'>Add to Cart</h2>
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Calculation from '../Calculation/Calculation';
+import { faCoffee, faDumbbell, faMasksTheater } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Card from '../Card/Card';
 import './Home.css';
 
 const Home = () => {
+    const[activities,setActivities]=useState([]);
+
+    useEffect(()=>{
+        fetch('activity.json')
+        .then(res=>res.json())
+        .then(data=>setActivities(data))
+    },[]);
+    
+console.log(activities);
     return (
-        <div className='activity-container'>
-            <div>
-                <Card></Card>
+        <div className='activities-container'>
+            <div className='card-section'>
+                <div className='header-container'>
+                    <div className='title'>
+                        <FontAwesomeIcon className='icon' icon={faMasksTheater}></FontAwesomeIcon>
+                        <h1>Group of Impetuous</h1>
+                    </div>       
+                        <h2>Select today's activity</h2>
+                </div>
+                <div className='card-container'>
+                    {
+                        activities.map(activity=><Card
+                        key={activity.id}
+                        activity={activity}
+                        ></Card>)
+                    }
+                </div>
             </div>
             <div>
                 <Calculation></Calculation>
