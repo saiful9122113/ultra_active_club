6ba996dc1e6601913e23f686713026c76f770a1a
Md. Saiful Islam <saiful9122113@gmail.com>
2022-09-29 11:34:10 +0600

    profile part is added in here

--- src/components/Calculation/Calculation.js
+++ src/components/Calculation/Calculation.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Calculation.css';
+import { faDumbbell, faLocationDot, faMasksTheater } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Calculation = () => {
     return (
-        <div>
-            <h3>THis is from calculation</h3>
-        </div>
+       <div>
+            <div className='profile'>
+                <img src="saiful.png" alt="" />
+                <div>
+                    <h1 className='profile-text'>Md. Saiful Islam</h1>
+                    <div className='icon-place-activity'>
+                        <FontAwesomeIcon className='icon' icon={faLocationDot}></FontAwesomeIcon>
+                        <p>Gopalganj,Dhaka</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h1>53</h1>
+                </div>
+            </div>
+
+       </div>
     );
 };
 
